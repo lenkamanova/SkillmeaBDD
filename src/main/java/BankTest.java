c1e6413bc330a1bdc7e48dc03f23b4e99d25d5dd
lenkamanova <lenka.manova@volny.cz>
2025-07-01 10:57:14 +0200

    init commit

--- src/main/java/BankTest.java
+++ src/main/java/BankTest.java
@@ -1,6 +1,8 @@
 import Product.BankAccount;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -15,5 +17,13 @@ public class BankTest {
     @Then("A user account balance is {int} kc")
     public void aUserAccountBalanceIsKc(int expectedBalance) {
         assertEquals(expectedBalance, firstBankAccount.getAccountBalance());
+    }
+
+    @When("A user withdraw {int} kc")
+    public void aUserWithdrawKc(int arg0) {
+    }
+
+    @And("A user transfer {int} kc to this account")
+    public void aUserTransferKcToThisAccount(int arg0) {
     }
 }
