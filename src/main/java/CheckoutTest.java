8a26dffd093c5392342f61e769c3332874dfb035
lenkamanova <lenka.manova@volny.cz>
2025-07-01 13:59:06 +0200

    init commit

--- src/main/java/CheckoutTest.java
+++ src/main/java/CheckoutTest.java
@@ -1,17 +1,27 @@
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import Product.Checkout;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CheckoutTest {
+
+    Checkout myCheckout;
+
     @Given("the price of a {string} is {int}c")
-    public void thePriceOfAIsC(String arg0, int arg1) {
+    public void thePriceOfAIsC(String name, int price) {
+        myCheckout = new Checkout();
+        myCheckout.addItem(name, price);
     }
 
     @When("A user checkout {string} {int}")
-    public void aUserCheckout(String arg0, int arg1) {
+    public void aUserCheckout(String name, int count) {
+        myCheckout.scanItems(name, count);
     }
 
     @Then("teh total price should be {int}c")
-    public void tehTotalPriceShouldBeC(int arg0) {
+    public void tehTotalPriceShouldBeC(int expectdTotalPrice) {
+        assertEquals(expectdTotalPrice, myCheckout.totalCart());
     }
 }
