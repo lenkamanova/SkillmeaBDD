a24485eaeb48b5a37ac655162adda02a0ddb290a
lenkamanova <lenka.manova@volny.cz>
2025-07-01 10:03:11 +0200

    init commit

--- src/main/java/BankTest.java
+++ src/main/java/BankTest.java
@@ -1,12 +1,17 @@
+import Product.BankAccount;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 
 public class BankTest {
+    BankAccount firstBankAccount;
+
     @Given("Bank account with {int} kc")
-    public void bankAccountWithKc(int arg0) {
+    public void bankAccountWithKc(int initialBalance) {
+        firstBankAccount = new BankAccount(initialBalance);
     }
 
     @Then("A user acount balance is {int} kc")
-    public void aUserAcountBalanceIsKc(int arg0) {
+    public void aUserAcountBalanceIsKc(int expectedBalance) {
+
     }
 }
